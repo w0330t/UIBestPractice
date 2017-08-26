fa8864276f2d96682760c9d4cfc2844242feed32
Blueness <w0330t@gmail.com>
2017-08-26 15:30:44 +0800

    Initial commit

---
+++ app/src/main/java/net/blueness/uibestpractice/Msg.kt
@@ -0,0 +1,10 @@
+package net.blueness.uibestpractice
+
+/**
+ * Created by Blueness on 2017/8/26.
+ */
+
+class Msg(content: String, type: Int){
+    val FINAL_TYPE_RECEIVED = 0
+    val FINAL_TYPE_SENT = 1
+}
---
+++ app/src/main/java/net/blueness/uibestpractice/MsgAdapter.kt
@@ -0,0 +1,31 @@
+package net.blueness.uibestpractice
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by Blueness on 2017/8/26.
+ */
+class MsgAdapter(mMsgList: MutableList<Msg>): RecyclerView.Adapter<MsgAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder{
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+
+}
