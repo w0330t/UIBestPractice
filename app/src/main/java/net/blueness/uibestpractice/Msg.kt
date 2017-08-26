81885e3e799a1cbf9941c72ca0cb9d7ed2382054
Blueness <w0330t@gmail.com>
2017-08-26 16:05:08 +0800

    Coding……

--- app/src/main/java/net/blueness/uibestpractice/Msg.kt
+++ app/src/main/java/net/blueness/uibestpractice/Msg.kt
@@ -4,7 +4,7 @@ package net.blueness.uibestpractice
  * Created by Blueness on 2017/8/26.
  */
 
-class Msg(content: String, type: Int){
+class Msg(val content: String, val type: Int){
     val FINAL_TYPE_RECEIVED = 0
     val FINAL_TYPE_SENT = 1
 }
--- app/src/main/java/net/blueness/uibestpractice/MsgAdapter.kt
+++ app/src/main/java/net/blueness/uibestpractice/MsgAdapter.kt
@@ -1,31 +1,40 @@
 package net.blueness.uibestpractice
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.jetbrains.anko.find
 
 /**
  * Created by Blueness on 2017/8/26.
  */
-class MsgAdapter(mMsgList: MutableList<Msg>): RecyclerView.Adapter<MsgAdapter.ViewHolder>(){
+class MsgAdapter(private val mMsgList: MutableList<Msg>): RecyclerView.Adapter<MsgAdapter.ViewHolder>(){
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
+        val leftLayout: LinearLayout = view.find(R.id.left_layout)
+        val rightLayout: LinearLayout = view.find(R.id.right_layout)
+        val leftMsg: TextView = view.find(R.id.left_msg)
+        val rightMsg: TextView = view.find(R.id.right_msg)
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder{
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.msg_item, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val msg = mMsgList.get(position)
+//        if(msg.type == Msg.)
     }
 
 
 
     override fun getItemCount(): Int {
-        return 0
+        return mMsgList.size
     }
 
 }
